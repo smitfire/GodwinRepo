892f9abcf96c4efef912090f9bbc73181820223f
Nick Smit <nick@g3s.ch>
2014-04-09 23:29:25 +0200

    added the routes and the case statements for sign in and out

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@posts = Post.find(:all, limit: 9)
+  	
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,13 @@ class PostsController < ApplicationController
   	
   end
 
+  def create
+  	@post = Post.create(url: params[:post][:url],category: params[:post][:category],accused: params[:post][:accused],accuser: params[:post][:accuser], quote: params[:post][:quote], excerpt: params[:post][:excerpt], user_id: current_user.id )
+  	Tag.create(title: params[:post][:category])
+  	Tag.create(title: params[:post][:accuser])
+  	Tag.create(title: params[:post][:accused])
+  	redirect_to post_path(@post.id)
+  end
   def show
   	@post = Post.find(params[:id])
   	@tag_accuser = Tag.find_by_title(@post.accuser)
