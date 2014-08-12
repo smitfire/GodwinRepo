97d7b1e493b0445a514a739482d97d86dadaa6cd
Nick Smit <nick.smit@gmail.com>
2014-08-12 13:33:20 +0200

    fixed the user profile page info detail for chart layout

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,258 +8,6 @@
     var DURATION = 1500;
     var DELAY = 500;
 
-    /**
-     * draw the fancy line chart
-     *
-     * @param {String} elementId elementId
-     * @param {Array}  data      data
-     */
-    function drawLineChart(elementId, data) {
-
-        var parse = d3.time.format('%Y-%m-%d').parse;
-
-        data = data.map(function(datum) {
-            datum.date = parse(datum.date);
-
-            return datum;
-        });
-
-        console.log(data);
-
-        var containerEl = document.getElementById(elementId),
-            width = containerEl.clientWidth,
-            height = width * 0.2,
-            margin = {
-                top: 20,
-                right: 10,
-                left: 10
-            },
-
-            detailWidth = 98,
-            detailHeight = 55,
-            detailMargin = 10,
-
-            container = d3.select(containerEl),
-            svg = container.select('svg')
-            .attr('width', width)
-            .attr('height', height + margin.top),
-
-            x = d3.time.scale().range([0, width - detailWidth]),
-            xAxis = d3.svg.axis().scale(x)
-            .ticks(8)
-            .tickSize(-height),
-            xAxisTicks = d3.svg.axis().scale(x)
-            .ticks(16)
-            .tickSize(-height)
-            .tickFormat(''),
-            y = d3.scale.linear().range([height, 0]),
-            yAxisTicks = d3.svg.axis().scale(y)
-            .ticks(12)
-            .tickSize(width)
-            .tickFormat('')
-            .orient('right'),
-
-            area = d3.svg.area()
-            .interpolate('linear')
-            .x(function(d) {
-                return x(d.date) + detailWidth / 2;
-            })
-            .y0(height)
-            .y1(function(d) {
-                return y(d.value);
-            }),
-
-            line = d3.svg.line()
-            .interpolate('linear')
-            .x(function(d) {
-                return x(d.date) + detailWidth / 2;
-            })
-            .y(function(d) {
-                return y(d.value);
-            }),
-
-            startData = data.map(function(datum) {
-                return {
-                    date: datum.date,
-                    value: 0
-                };
-            }),
-
-            circleContainer;
-
-
-        x.domain([data[0].date, data[data.length - 1].date]);
-
-        y.domain([0, d3.max(data, function(d) {
-            return d.value;
-        }) + 700]);
-
-        svg.append('g')
-            .attr('class', 'lineChart--xAxisTicks')
-            .attr('transform', 'translate(' + detailWidth / 2 + ',' + height + ')')
-            .call(xAxisTicks);
-
-        svg.append('g')
-            .attr('class', 'lineChart--xAxis')
-            .attr('transform', 'translate(' + detailWidth / 2 + ',' + (height + 7) + ')')
-            .call(xAxis);
-
-        svg.append('g')
-            .attr('class', 'lineChart--yAxisTicks')
-            .call(yAxisTicks);
-
-
-        svg.append('path')
-            .datum(startData)
-            .attr('class', 'lineChart--areaLine')
-            .attr('d', line)
-            .transition()
-            .duration(DURATION)
-            .delay(DURATION / 2)
-            .attrTween('d', tween(data, line))
-            .each('end', function() {
-                drawCircles(data);
-            });
-
-
-
-        svg.append('path')
-            .datum(startData)
-            .attr('class', 'lineChart--area')
-            .attr('d', area)
-            .transition()
-            .duration(DURATION)
-            .attrTween('d', tween(data, area));
-
-
-        function drawCircle(datum, index) {
-            circleContainer.datum(datum)
-                .append('circle')
-                .attr('class', 'lineChart--circle')
-                .attr('r', 0)
-                .attr(
-                    'cx',
-                    function(d) {
-                        return x(d.date) + detailWidth / 2;
-                    }
-            )
-                .attr(
-                    'cy',
-                    function(d) {
-                        return y(d.value);
-                    }
-            )
-                .on('mouseenter', function(d) {
-                    d3.select(this)
-                        .attr(
-                            'class',
-                            'lineChart--circle lineChart--circle__highlighted'
-                    )
-                        .attr('r', 7);
-
-                    d.active = true;
-
-                    showCircleDetail(d);
-                })
-                .on('mouseout', function(d) {
-                    d3.select(this)
-                        .attr(
-                            'class',
-                            'lineChart--circle'
-                    )
-                        .attr('r', 6);
-
-                    if (d.active) {
-                        hideCircleDetails();
-
-                        d.active = false;
-                    }
-                })
-                .on('click touch', function(d) {
-                    if (d.active) {
-                        showCircleDetail(d);
-                    } else {
-                        hideCircleDetails();
-                    }
-                })
-                .transition()
-                .delay(DURATION / 10 * index)
-                .attr('r', 6);
-        }
-
-        function drawCircles(data) {
-            circleContainer = svg.append('g');
-
-            data.forEach(function(datum, index) {
-                drawCircle(datum, index);
-            });
-        }
-
-        function hideCircleDetails() {
-            circleContainer.selectAll('.lineChart--bubble')
-                .remove();
-        }
-
-        function showCircleDetail(data) {
-            var details = circleContainer.append('g')
-                .attr('class', 'lineChart--bubble')
-                .attr(
-                    'transform',
-                    function() {
-                        var result = 'translate(';
-
-                        result += x(data.date);
-                        result += ', ';
-                        result += y(data.value) - detailHeight - detailMargin;
-                        result += ')';
-
-                        return result;
-                    }
-                );
-
-            details.append('path')
-                .attr('d', 'M2.99990186,0 C1.34310181,0 0,1.34216977 0,2.99898218 L0,47.6680579 C0,49.32435 1.34136094,50.6670401 3.00074875,50.6670401 L44.4095996,50.6670401 C48.9775098,54.3898926 44.4672607,50.6057129 49,54.46875 C53.4190918,50.6962891 49.0050244,54.4362793 53.501875,50.6670401 L94.9943116,50.6670401 C96.6543075,50.6670401 98,49.3248703 98,47.6680579 L98,2.99898218 C98,1.34269006 96.651936,0 95.0000981,0 L2.99990186,0 Z M2.99990186,0')
-                .attr('width', detailWidth)
-                .attr('height', detailHeight);
-
-            var text = details.append('text')
-                .attr('class', 'lineChart--bubble--text');
-
-            text.append('tspan')
-                .attr('class', 'lineChart--bubble--label')
-                .attr('x', detailWidth / 2)
-                .attr('y', detailHeight / 3)
-                .attr('text-anchor', 'middle')
-                .text(data.label);
-
-            text.append('tspan')
-                .attr('class', 'lineChart--bubble--value')
-                .attr('x', detailWidth / 2)
-                .attr('y', detailHeight / 4 * 3)
-                .attr('text-anchor', 'middle')
-                .text(data.value);
-        }
-
-        function tween(b, callback) {
-            return function(a) {
-                var i = (function interpolate() {
-                    return function(t) {
-                        return a.map(function(datum, index) {
-                            return {
-                                date: datum.date,
-                                value: datum.value + b[index].value * t
-                            };
-                        });
-                    };
-                })();
-
-                return function(t) {
-                    return callback(i(t));
-                };
-            };
-        }
-    }
-
     /*
     draw the fancy pie chart
   
@@ -277,8 +25,8 @@
             radius = Math.min(width, height) / 2,
             container = d3.select(containerEl),
             svg = container.select('svg')
-            .attr('width', width)
-            .attr('height', height);
+                        .attr('width', width)
+                        .attr('height', height);
         var pie = svg.append('g')
             .attr(
                 'transform',
@@ -360,8 +108,11 @@
                 anchor,
                 infoContainer,
                 position;
+                // console.log(bBox.x);
+                // console.log(bBox.width);
+                console.log( bBox.x + (bBox.width - 1) );
 
-            if ((bBox.x + bBox.width / 2) > 0) {
+            if ( bBox.x + (bBox.width - 1) < 0) {
                 infoContainer = detailedInfo.append('g')
                     .attr('width', infoWidth)
                     .attr(
@@ -381,20 +132,12 @@
                 position = 'left';
             }
 
-            infoContainer.data([data.title])
-                .append('text')
-                .text(data.title)
-                .attr('class', 'pieChart--detail--textContainer')
-                .attr('x', (position === 'left' ? 20 : infoWidth - 20))
-                .attr('y', 20)
-                .attr('text-anchor', anchor);
-
             infoContainer.data([data.value])
                 .append('text')
                 .text('0 ')
                 .attr('class', 'pieChart--detail--percentage')
-                .attr('x', (position === 'left' ? 0 : infoWidth))
-                .attr('y', 0)
+                .attr('x', (position === 'left' ? 0 : infoWidth - 40) )
+                .attr('y', (position === 'left' ? 0 : height-213) )
                 .attr('text-anchor', anchor)
                 .transition()
                 .duration(DURATION)
@@ -402,11 +145,18 @@
                     var i = d3.interpolateRound(+this.textContent.replace(/\s%/ig, ''),
                         d
                     );
-
                     return function(t) {
                         this.textContent = i(t) + "      " + data.description;
                     };
                 });
+            
+            infoContainer.data([data.title])
+                .append('text')
+                .text(data.title)
+                .attr('class', 'pieChart--detail--textContainer')
+                .attr('x', (position === 'left' ? 20 : infoWidth - 60))
+                .attr('y', (position === 'left' ? 50 : height-163) )
+                .attr('text-anchor', anchor);
 
             infoContainer.append('line')
                 .attr('class', 'pieChart--detail--divider')
@@ -420,8 +170,7 @@
             // infoContainer.data( [ data.description ] )
             //               .append( 'foreignObject' )
             //               .attr( 'width', infoWidth  )
-            //               // .attr( 'x', ( position === 'left' ? 0 : infoWidth - 250 ) )
-            //               .attr( 'height', 150 )
+            //               .attr( 'height', 100 )
             //               .append( 'xhtml:body' )
             //               .attr(
             //                 'class',
