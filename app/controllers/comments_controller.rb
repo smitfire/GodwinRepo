f27b4bac5af9ae4c4aa78350e575fe50f0b182cf
Nick Smit <nick.smit@gmail.com>
2014-08-14 14:15:41 +0200

    added the form to create and delete on post show page

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to post_path(comment.post.id)    
+  end
+
+  def destroy
+      Comment.destroy(params[:id])
+      redirect_to post_path(params[:post_id])
+  end
+  
+  private
+    def comment_params
+      params.require(:comment).permit(:content, :user_id, :post_id)
+    end
+end
