914683ebe894e9020ce48029ca08b0ada8f55bd3
Nick Smit <nick.smit@gmail.com>
2014-08-19 18:50:14 +0200

    added search by accuser category and accused category

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,14 @@ class SearchController < ApplicationController
   end
 
   def results
-    accuser = Accuser.find_by(title: params[:accuser])
-    accuser = Accused.find_by(title: params[:accused])
-    @posts = Post.where(accuser_id: accuser.id, accused_id: accused.id).all
-    
+    # accuser = Accuser.find_by(title: params[:accuser])
+    # accused = Accused.find_by(title: params[:accused])
+    accuser_category_posts  = Category.find(params[:accuser_category]).accuser_posts
+    accusedd_category_posts = Category.find(params[:accused_category]).accused_posts
+    @posts = []
+    (accuser_category_posts & accusedd_category_posts).each do |post|
+      @posts << post
+    end
     render 'index'
   end
 
